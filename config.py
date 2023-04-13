19eca6973a010f9c264d7650801c00d3cdc68866
AndrewYeo03 <109736223+AndrewYeo03@users.noreply.github.com>
2023-04-13 10:46:06 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "database-1.ctqdzmxmliea.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "yeojunken-employee"
+customregion = "us-east-1"
